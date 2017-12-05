1b1c229d01e16d0e90c0853f91eb72d933a6d3f1
Mads <lxzmads@gmail.com>
2017-12-05 10:21:57 +0800

    Add class service.

--- src/main/java/xmu/crms/view/ClassController.java
+++ src/main/java/xmu/crms/view/ClassController.java
@@ -4,84 +4,224 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.omg.CORBA.Request;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import xmu.crms.entity.Class;
+import xmu.crms.entity.Group;
+import xmu.crms.entity.User;
+import xmu.crms.service.ClassService;
+import xmu.crms.service.ClassServiceImpl;
 
+/**
+ * Class Controller
+ * @author Xuezhang.Liu
+ */
 @Controller
 
 @RequestMapping("/class")
 
 public class ClassController {
 
+	@Autowired
+	private ClassService classService = new ClassServiceImpl();
+
 	@RequestMapping(method = GET)
-	public String getUserClass(String courseName, String teacherName, Model model) {
-		
-		return null;
+	@ResponseBody
+	public ResponseEntity<List<Class>> getClassesByUserId(@RequestParam(value = "courseName", required = false) String courseName,
+														  @RequestParam(value = "courseTeacher", required = false) String teacherName) {
+
+
+		int userId = 2777;
+
+		List<Class> classes = classService.getClassListByUserId(userId, courseName, teacherName);
+		if(null == classes){
+			return ResponseEntity.status(404).build();
+		}else{
+			return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(classes);
+		}
 	}
 	
 	@RequestMapping(value="/{classId}", method = GET)
-	public String getClassById(@PathVariable int id, Model model) {
+	@ResponseBody
+	public ResponseEntity<Class> getClassById(@PathVariable int classId) {
+		// id 格式判断 400
 
-		
-		return null;
+		Class myClass = classService.getClassById(classId);
+		if(null == myClass){
+			// 未找到班级
+			return ResponseEntity.status(404).build();
+		}else{
+			return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(myClass);
+		}
 	}
 	
 	@RequestMapping(value="/{classId}", method = PUT)
-	public String updateClassById(@PathVariable int id) {
-		
-		return null;
+	@ResponseBody
+	public ResponseEntity updateClassById(@PathVariable int classId, @RequestParam Class myClass) {
+
+		// id格式判断 400
+		// 权限认证 403
+
+		Boolean result = classService.updateClassById(classId, myClass);
+		if(result){
+			return ResponseEntity.status(204).build();
+		}else{
+			//未找到班级 404
+			return ResponseEntity.status(404).build();
+		}
 	}
 	
 	@RequestMapping(value="/{classId}", method = DELETE)
-	public String deleteClassById(@PathVariable int id) {
-		
-		return null;
+	@ResponseBody
+	public ResponseEntity deleteClassById(@PathVariable int classId) {
+		// id格式判断 400
+		// 权限认证 403
+
+		Boolean result = classService.deleteClassById(classId);
+		if(result){
+			return ResponseEntity.status(204).build();
+		}else{
+			return ResponseEntity.status(404).build();
+		}
 	}
 	
 	@RequestMapping(value="/{classId}/student", method = GET)
-	public String getClassStudent(@PathVariable int id) {
-		
-		return null;
+	@ResponseBody
+	public ResponseEntity<List<User>> getStudentListByClassId(@PathVariable int classId,
+										  @RequestParam(value = "numBeginWith",required = false) String numBeginWith,
+										  @RequestParam(value = "nameBeginWith",required = false) String nameBeginWith)
+	{
+		// id格式判断 400
+		// 权限认证 403
+
+		List<User> students = classService.getStudentListByClassId(classId,numBeginWith, nameBeginWith);
+
+		if(null == students){
+			// 未找到班级404
+			return ResponseEntity.status(404).build();
+		}else{
+			return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(students);
+		}
 	}
 	
 	@RequestMapping(value="/{classId}/student", method = POST)
-	public String selectClass(@PathVariable int id) {
-		
-		return null;
+	@ResponseBody
+	public ResponseEntity selectClass(@PathVariable int classId) {
+		// id格式判断 400
+		// 权限认证 403
+
+		int userId = 123;
+		Boolean result = classService.addStudentToClassByClassIdAndStudentId(classId, userId);
+		if(result){
+			return ResponseEntity.status(201).contentType(MediaType.APPLICATION_JSON_UTF8).body(new Object(){public String url = "/class/"+classId+"/student/2757";});
+
+		}else{
+			// 选过
+			return ResponseEntity.status(409).build();
+		}
+
 	}
 	
 	@RequestMapping(value="/{classId}/student/{studentId}", method = DELETE)
-	public String deselectClass() {
-		
-		return null;
-	}
-	
-	@RequestMapping(value="/{classId}/attendance", method = GET)
-	public String getAttendanceByClassId() {
-		
-		return null;
-	}
-	
-	@RequestMapping(value="/{classId}/attendance/{studentId}", method = PUT)
-	public String updateAttendanceByClassId() {
-		
-		return null;
+	@ResponseBody
+	public ResponseEntity deSelectClass(@PathVariable int classId, @PathVariable int studentId) {
+		// id格式判断 400
+		// 权限认证 403
+
+		Boolean result = classService.deleteStudentFromClassByClassIdAndStudentId(classId, studentId);
+
+		if(result){
+			return ResponseEntity.status(204).build();
+		}else{
+			// 不存在这个选课或不存在这个学生、班级
+			return ResponseEntity.status(404).build();
+		}
 	}
 	
 	@RequestMapping(value="/{classId}/classgroup", method = GET)
-	public String updateClassGroupByClassId() {
-		
-		return null;
+	@ResponseBody
+	public ResponseEntity<Group> getGroupByClassId(@PathVariable int classId) {
+		// id格式判断 400
+		// 权限认证 403
+		int userId = 2777;
+
+		Group group = classService.getGroupByClassId(classId, userId);
+		if(null == group){
+			// 还没有小组 404
+			return ResponseEntity.status(404).build();
+		}else{
+			return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(group);
+		}
 	}
 	
-	@RequestMapping(value="/{classId}/classgroup", method = PUT)
-	public String getClassGroupByClassId() {
-		
-		return null;
+	@RequestMapping(value="/{classId}/classgroup/resign", method = PUT)
+	public ResponseEntity resignFromGroup(@PathVariable int classId, @RequestBody Map<String, Object> request) {
+
+		// id格式判断 400
+		// 权限认证 403
+
+		Boolean result = classService.deleteLeaderFromGroupByClassId(classId,Integer.parseInt(request.get("groupId").toString()));
+		if(result){
+			return ResponseEntity.status(204).build();
+		}else {
+			// 失败
+			return ResponseEntity.status(404).build();
+		}
+	}
+	
+	@RequestMapping(value="/{classId}/classgroup/assign", method = GET)
+	public ResponseEntity updateClassGroupByClassId(@PathVariable int classId, @RequestBody Map<String, Object> request) {
+		// id格式判断 400
+		// 权限认证 403
+
+		Boolean result = classService.addLeaderInGroupByClassId(classId,
+				Integer.parseInt(request.get("id").toString()),
+				Integer.parseInt(request.get("groupId").toString()));
+		if(result){
+			return ResponseEntity.status(204).build();
+		}else {
+			// 已经有组长
+			return ResponseEntity.status(409).build();
+		}
+	}
+	
+	@RequestMapping(value="/{classId}/classgroup/add", method = PUT)
+	public ResponseEntity addMemberInGroup(@PathVariable int classId, @RequestBody Map<String, Object> request) {
+		// id格式判断 400
+		// 权限认证 403
+
+		Boolean result = classService.addMemberInGroupByClassId(classId,
+				Integer.parseInt(request.get("id").toString()),
+				Integer.parseInt(request.get("groupId").toString()));
+		if(result){
+			return ResponseEntity.status(204).build();
+		}else {
+			// 已经在小组
+			return ResponseEntity.status(409).build();
+		}
+	}
+
+	@RequestMapping(value="/{classId}/classgroup/add", method = PUT)
+	public ResponseEntity deleteMemberFromGroup(@PathVariable int classId, @RequestBody Map<String, Object> request) {
+		// id格式判断 400
+		// 权限认证 403
+
+		Boolean result = classService.deleteMemberFromGroupByClassId(classId,
+				Integer.parseInt(request.get("id").toString()),
+				Integer.parseInt(request.get("groupId").toString()));
+		if(result){
+			return ResponseEntity.status(204).build();
+		}else {
+			// 待移除的学生不在组内
+			return ResponseEntity.status(409).build();
+		}
 	}
 	
 	
