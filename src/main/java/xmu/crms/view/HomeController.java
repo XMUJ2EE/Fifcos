3114d6d7d7351361a3608706c4199043ccfd62ee
Mads <lxzmads@gmail.com>
2017-12-06 11:27:56 +0800

    Add templates.

--- src/main/java/xmu/crms/view/HomeController.java
+++ src/main/java/xmu/crms/view/HomeController.java
@@ -1,12 +1,16 @@
-//package xmu.crms.view;
-//
-//import org.springframework.stereotype.Controller;
-//
-//@Controller
-//public class HomeController {
-//
-//    @RequestMapping(value = "/", method = GET)
-//    public getHome(){
-//        return
-//    }
-//}
+package xmu.crms.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author mads
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/login")
+    public String login(){
+        return "common/AccountLoginPage";
+    }
+}
--- src/main/java/xmu/crms/view/MvcConfigurer.java
+++
@@ -1,6 +0,0 @@
-package xmu.crms.view;
-
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-public class MvcConfigurer extends WebMvcConfigurerAdapter{
-}
