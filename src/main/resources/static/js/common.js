7efec2534dc5d36f7d934d29e7540d83254485a4
muzinicemoon <1437525521@qq.com>
2017-12-20 23:20:56 +0800

    hh

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,26 +1,28 @@
 
-// function login(){//AccountLoginPage
-//     window.location.href="../student/StudentHomePage.html";
-//     var ata = {phone:$("#username").val(),password:$("#password").val()}
-//     $.ajax({
-//         type:'post',
-//         url: '/signin',
-//         dataType: "json",
-//         contentType: "application/json;",
-//         data: JSON.stringify(ata),
-//         success: function (data,status) {
-//             if(status == "OK"){//状态码存疑
-//                 if(data.type == "student")
-//             	    window.location.href="StudentHomePage.html";
-//                 else
-//                     window.location.href="TeacherHomePage.html";
-//             }
-//             else{
-//             	alert("手机号密码错误！");
-//             }
-//         }
-//     });
-// }
+function login(){//AccountLoginPage
+    window.location.href="../student/StudentHomePage.html";
+    var ata = {phone:$("#username").val(),password:$("#password").val()}
+    $.ajax({
+        type:'post',
+        url: '/signin',
+        dataType: "json",
+        contentType: "application/json;",
+        data: JSON.stringify(ata),
+        success: function (data,status,xhr) {
+            if(xhr.status == 200){//状态码存疑
+                if(data.type == "student"){
+            	    window.location.href="student/profile";
+                alert("手机号密码错误！");}
+                else
+                    window.location.href="teacher/home";
+                alert("手机号密码错误！");
+            }
+            else{
+            	alert("手机号密码错误！");
+            }
+        }
+    });
+}
 
 // function register(){//RegisterPage
 //     window.location.href="login.html";
