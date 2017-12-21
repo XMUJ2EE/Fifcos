3ee7e4e829c2836298e8b514c0f80fe429a69041
WongGamTou <286963982@qq.com>
2017-12-21 21:22:54 +0800

    course

--- src/main/java/xmu/crms/view/CourseController.java
+++ src/main/java/xmu/crms/view/CourseController.java
@@ -31,28 +31,54 @@ public class CourseController {
 	@ResponseBody
 	public ResponseEntity getUserCourses() {
 
-		return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(null);
+		String a = "[\n" +
+				"  {\n" +
+				"    \"id\": 1,\n" +
+				"    \"name\": \"OOAD\",\n" +
+				"    \"numClass\": 3,\n" +
+				"    \"numStudent\": 60,\n" +
+				"    \"startTime\": \"2017-9-1\",\n" +
+				"    \"endTime\": \"2018-1-1\"\n" +
+				"  },\n" +
+				"  {\n" +
+				"    \"id\": 2,\n" +
+				"    \"name\": \"J2EE\",\n" +
+				"    \"numClass\": 1,\n" +
+				"    \"numStudent\": 60,\n" +
+				"    \"startTime\": \"2017-9-1\",\n" +
+				"    \"endTime\": \"2018-1-1\"\n" +
+				"  }\n" +
+				"]";
+		return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(a);
 	}
 
 	@RequestMapping(method = POST)
 	@ResponseBody
 	public ResponseEntity createCourse() {
-
-		return ResponseEntity.status(201).contentType(MediaType.APPLICATION_JSON_UTF8).body(null);
+		String a = "{\n" +
+				"  \"id\": 23\n" +
+				"}";
+		return ResponseEntity.status(201).contentType(MediaType.APPLICATION_JSON_UTF8).body(a);
 	}
 	
 	@RequestMapping(value = "/{courseId}", method = GET)
 	@ResponseBody
 	public ResponseEntity getCourseById(@PathVariable int courseId) {
-
-		return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(null);
+		String a = "{\n" +
+				"  \"id\": 23,\n" +
+				"  \"name\": \"OOAD\",\n" +
+				"  \"description\": \"面向对象分析与设计\",\n" +
+				"  \"teacherName\": \"邱明\",\n" +
+				"  \"teacherEmail\": \"mingqiu@xmu.edu.cn\"\n" +
+				"}";
+		return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(a);
 		
 	}
 	
 	@RequestMapping(value = "/{courseId}", method = PUT)
 	@ResponseBody
 	public ResponseEntity updateCourseById(@PathVariable int courseId) {
-
+		String a = "";
 		return ResponseEntity.status(204).build();
 
 	}
@@ -60,15 +86,24 @@ public class CourseController {
 	@RequestMapping(value = "/{courseId}", method = DELETE)
 	@ResponseBody
 	public ResponseEntity deleteCourseById(@PathVariable int courseId) {
-
+		String a = "";
 		return ResponseEntity.status(204).build();
 	}
 	
 	@RequestMapping(value = "/{courseId}/class", method = GET)
 	@ResponseBody
 	public ResponseEntity getClassListByCourseId(@PathVariable int courseId) {
-
-		return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(null);
+		String a = "[\n" +
+				"  {\n" +
+				"    \"id\": 45,\n" +
+				"    \"name\": \"周三1-2节\"\n" +
+				"  },\n" +
+				"  {\n" +
+				"    \"id\": 48,\n" +
+				"    \"name\": \"周三3-4节\"\n" +
+				"  }\n" +
+				"]";
+		return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(a);
 
 	}
 	
@@ -76,40 +111,99 @@ public class CourseController {
 	@RequestMapping(value = "/{courseId}/class", method = POST)
 	@ResponseBody
 	public ResponseEntity createClassByCourseId(@PathVariable int courseId) {
-
-		return ResponseEntity.status(201).contentType(MediaType.APPLICATION_JSON_UTF8).body(null);
+		String a = "{\n" +
+				"  \"id\": 45\n" +
+				"}";
+		return ResponseEntity.status(201).contentType(MediaType.APPLICATION_JSON_UTF8).body(a);
 	}
 	
 	@RequestMapping(value = "/{courseId}/seminar", method = GET)
 	@ResponseBody
 	public ResponseEntity getSeminarsByCourseId(@PathVariable int courseId) {
-
-		return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(null);
+		String a = "[\n" +
+				"  {\n" +
+				"    \"id\": 29,\n" +
+				"    \"name\": \"界面原型设计\",\n" +
+				"    \"description\": \"界面原型设计\",\n" +
+				"    \"groupingMethod\": \"fixed\",\n" +
+				"    \"startTime\": \"2017-09-25\",\n" +
+				"    \"endTime\": \"2017-10-09\",\n" +
+				"    \"grade\": 4\n" +
+				"  },\n" +
+				"  {\n" +
+				"    \"id\": 32,\n" +
+				"    \"name\": \"概要设计\",\n" +
+				"    \"description\": \"模型层与数据库设计\",\n" +
+				"    \"groupingMethod\": \"fixed\",\n" +
+				"    \"startTime\": \"2017-10-10\",\n" +
+				"    \"endTime\": \"2017-10-24\",\n" +
+				"    \"grade\": 5\n" +
+				"  }\n" +
+				"]";
+		return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(a);
 	}
 	
 	@RequestMapping(value = "/{courseId}/seminar", method = POST)
 	@ResponseBody
 	public ResponseEntity createSeminarByCourseId(@PathVariable int courseId) {
-
-		return ResponseEntity.status(201).contentType(MediaType.APPLICATION_JSON_UTF8).body(null);
+		String a = "{\n" +
+				"  \"id\": 32\n" +
+				"}";
+		return ResponseEntity.status(201).contentType(MediaType.APPLICATION_JSON_UTF8).body(a);
 	}
 
 	@RequestMapping(value = "/{courseId}/seminar/current", method = GET)
 	@ResponseBody
 	public ResponseEntity getCurrentSeminarByCourseId(@PathVariable int courseId){
-
-		return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(null);
+		String a = "{\n" +
+				"  \"id\": 29,\n" +
+				"  \"name\": \"界面原型设计\",\n" +
+				"  \"courseName\": \"OOAD\",\n" +
+				"  \"groupingMethod\": \"fixed\",\n" +
+				"  \"startTime\": \"2017-09-25\",\n" +
+				"  \"endTime\": \"2017-10-09\",\n" +
+				"  \"classes\": [\n" +
+				"    {\n" +
+				"      \"id\": 53,\n" +
+				"      \"name\": \"周三12\"\n" +
+				"    },\n" +
+				"    {\n" +
+				"      \"id\": 57,\n" +
+				"      \"name\": \"周三34\"\n" +
+				"    }\n" +
+				"  ]\n" +
+				"}";
+		return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(a);
 	}
 
 	@RequestMapping(value = "/{courseId}/grade", method = GET)
 	@ResponseBody
 	public ResponseEntity getAllGradeByCourseId(@PathVariable int courseId){
 
-		List<SeminarGroup> seminarGroups = gradeService.listSeminarGradeByCourseId(BigInteger.valueOf(courseId));
-		if (seminarGroups == null) {
-			return ResponseEntity.status(404).body(null);
-		}else{
-			return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(seminarGroups);
-		}
+//		List<SeminarGroup> seminarGroups = gradeService.listSeminarGradeByCourseId(BigInteger.valueOf(courseId));
+//		if (seminarGroups == null) {
+//			return ResponseEntity.status(404).body(null);
+//		}else{
+//			return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(seminarGroups);
+//		}
+		String a = "[\n" +
+				"  {\n" +
+				"    \"seminarName\": \"需求分析\",\n" +
+				"    \"groupName\": \"3A2\",\n" +
+				"    \"leaderName\": \"张三\",\n" +
+				"    \"presentationGrade\": 3,\n" +
+				"    \"reportGrade\": 4,\n" +
+				"    \"grade\": 4\n" +
+				"  },\n" +
+				"  {\n" +
+				"    \"seminarName\": \"界面原型设计\",\n" +
+				"    \"groupName\": \"3A3\",\n" +
+				"    \"leaderName\": \"张三\",\n" +
+				"    \"presentationGrade\": 4,\n" +
+				"    \"reportGrade\": 4,\n" +
+				"    \"grade\": 4\n" +
+				"  }\n" +
+				"]";
+		return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(a);
 	}
 }
--- src/main/java/xmu/crms/view/SeminarController.java
+++ src/main/java/xmu/crms/view/SeminarController.java
@@ -2,6 +2,7 @@ package xmu.crms.view;
 
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,19 +12,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import xmu.crms.entity.Seminar;
 import xmu.crms.entity.Topic;
+import xmu.crms.exception.SeminarNotFoundException;
+import xmu.crms.service.SeminarService;
 import xmu.crms.view.vo.*;
+
+import java.math.BigInteger;
 
 @Controller
 
 @RequestMapping("/seminar")
 
 public class SeminarController {
-	
+//	@Autowired
+//	SeminarService seminarService;
+
 	@RequestMapping(value = "/{seminarId}", method = GET)
 	@ResponseBody
 	public ResponseEntity getSeminarById(@PathVariable int seminarId) {
-
-		return ResponseEntity.status(200).body(null);
+		Seminar seminar = new Seminar();
+//		try {
+//			seminar = seminarService.getSeminarBySeminarId(BigInteger.valueOf(seminarId));
+//		} catch (SeminarNotFoundException e) {
+//			e.printStackTrace();
+//		}
+		if (seminar == null) {
+			return ResponseEntity.status(404).body(null);
+		}else {
+			return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(seminar);
+		}
 	}
 	
 	@RequestMapping(value = "/{seminarId}", method = PUT)
@@ -36,7 +52,18 @@ public class SeminarController {
 	
 	@RequestMapping(value = "/{seminarId}", method = DELETE)
 	public ResponseEntity deleteSeminarById(@PathVariable int seminarId) {
-
+//		Seminar seminar;
+//		try {
+//			seminar = seminarService.getSeminarBySeminarId(BigInteger.valueOf(seminarId));
+//			if (seminar == null) {
+//				return ResponseEntity.status(404).build();
+//			} else {
+//				seminarService.deleteSeminarBySeminarId(BigInteger.valueOf(seminarId));
+//				return ResponseEntity.status(204).build();
+//			}
+//		} catch (SeminarNotFoundException e) {
+//			e.printStackTrace();
+//		}
 		return ResponseEntity.status(204).build();
 	}
 	
