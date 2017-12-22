8c1ded7ecd81a7d15dae1d54e6897fcb154cd308
Mads <lxzmads@gmail.com>
2017-12-22 22:29:06 +0800

    Modified authentication redirect.

--- src/main/resources/static/js/jwt.js
+++ src/main/resources/static/js/jwt.js
@@ -3,6 +3,9 @@ $.ajaxSetup({
     beforeSend:function(xhr) {
         if(localStorage.jwt){
             xhr.setRequestHeader('Authorization','Bearer '+localStorage.jwt)
+        }else{
+            alert("未授权访问 !");
+            window.location.href = "/login";
         }
 
     }
