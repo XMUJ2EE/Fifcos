514a463e888a215d6ba4436d6290be24c5bf86e4
Mads <lxzmads@gmail.com>
2017-12-22 22:50:56 +0800

    Modified authentication redirect

--- src/main/resources/static/js/jwt.js
+++ src/main/resources/static/js/jwt.js
@@ -3,10 +3,16 @@ $.ajaxSetup({
     beforeSend:function(xhr) {
         if(localStorage.jwt){
             xhr.setRequestHeader('Authorization','Bearer '+localStorage.jwt)
-        }else{
-            alert("未授权访问 !");
+        }
+    },
+    statusCode:{
+       401: function () {
+            alert("未授权访问");
+            window.location.href = "/login";
+        } ,
+        403:function () {
+            alert("未授权访问");
             window.location.href = "/login";
         }
-
     }
 });
