d60f99c07d20d4e5ed942f6d400d3ef94206d8b9
WongGamTou <286963982@qq.com>
2017-12-27 02:43:38 +0800

    password encrypt

--- src/main/java/xmu/crms/security/FifcosAuthenticationProvider.java
+++ src/main/java/xmu/crms/security/FifcosAuthenticationProvider.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import xmu.crms.security.FifcosAuthenticationToken;
 import xmu.crms.security.UserDetailsImpl;
 import xmu.crms.security.UserDetailsServiceImpl;
@@ -28,8 +29,14 @@ public class FifcosAuthenticationProvider implements AuthenticationProvider{
         UserDetails userDetails;
         if(fifcosAuthenticationToken.getOpenid() == null) {
             // web端认证
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
             String username = fifcosAuthenticationToken.getPhone();
             userDetails = userDetailsService.loadUserByUsername(username);
+            System.out.println(fifcosAuthenticationToken.getPassword());
+            System.out.println(userDetails.getPassword());
+            System.out.println(encoder.encode("123"));
+            Integer a = 123;
+            System.out.println(encoder.encode(a.toString()));
             if (!userDetails.getPassword().equals(fifcosAuthenticationToken.getPassword())) {
                 throw new BadCredentialsException("密码错误");
             }
