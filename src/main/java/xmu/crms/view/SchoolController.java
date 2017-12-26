8aef3d6583d2dfe7b6502d2274194e0e2e19b71f
We Ian <We Ian>
2017-12-27 00:26:35 +0800

    前端页面问题修正

--- src/main/java/xmu/crms/view/SchoolController.java
+++ src/main/java/xmu/crms/view/SchoolController.java
@@ -7,6 +7,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import xmu.crms.entity.School;
 import xmu.crms.service.SchoolService;
@@ -29,7 +30,7 @@ public class SchoolController {
 
     @RequestMapping(method = GET)
     @ResponseBody
-    public ResponseEntity getSchoolList(@PathParam("city") String city) {
+    public ResponseEntity getSchoolList(@RequestParam("city") String city) {
         List<School> schools = schoolService.listSchoolByCity(city);
 
         return ResponseEntity.status(201).contentType(MediaType.APPLICATION_JSON_UTF8).body(schools);
@@ -54,7 +55,7 @@ public class SchoolController {
     }
     @RequestMapping(value = "/city", method = GET)
     @ResponseBody
-    public  ResponseEntity getCity(@PathParam("province") String province) {
+    public  ResponseEntity getCity(@RequestParam ("province") String province) {
         List<String> citys = new ArrayList<String>();
         citys = schoolService.listCity(province);
 
--- src/main/resources/static/js/TeacherCreateSchool.js
+++ src/main/resources/static/js/TeacherCreateSchool.js
@@ -1,61 +1,69 @@
+function schoolload(){
+     $.ajax({
+    type:'get',
+    url: '/school/province',
+    dataType: "json",
+    contentType: "application/json;",
+    success: function (data,textStatus,xhr) {
+        if(xhr.status == 200){
+            var content = document.getElementById("pro");
+            var str = "";
+            for(i=0;i<data.length;i++){
+                str = str +"<option>"+data[i]+"</option>"
+            }
+            content.innerHTML = str;
+            }
+    },
+    statusCode: {
+        400: function () {
+            alert("错误的ID格式");
+        },
+        404: function () {
+            alert("未找到话题");
+        }
+    }
+});
+
+}
+
+function find_city(){
+    $.ajax({
+        type: "get",
+        url: "/school/city?province="+$("#pro").find("option:selected").text(),
+        success: function(data) {
+            var content = document.getElementById("city");
+            var str = "";
+            for(i=0;i<data.length;i++){
+                str = str +"<option>"+data[i]+"</option>"
+            }
+            content.innerHTML = str;
+        }
+    });
+}
+
 function createSchoolsend(){
-    $.ajax({
-        type: "post",
-        url: "/school",
-        data:'name='+$("#name").val()+'&province='+$("#pro").val()+'&city='+$("#city").val(),
-        success: function(data) {
-            alert("success");
+     var ata = {
+        name:$("#schoolname").val(),
+        province:$("#pro").val(),
+        city:$('#city').val(),
 
-        }
-    });
-}
-
-function createSchool(){
-    $.ajax({
-        type: "get",
-        url: "/school/province",
-        success: function(data) {
-            alert("success");
-            var content = document.getElementById('pro');
-            content.empty();
-            var str = "";
-            for(i=0;i<data.length;i++){
-                str = str +"<option>"+data[i]+"</option>"
+    }
+       $.ajax({
+        type:'post',
+        url: '/school',
+        dataType: "json",
+        contentType: "application/json;",
+        data: JSON.stringify(ata),
+        success: function (data,textStatus,xhr) {
+            if(xhr.status==201){
+                alert("修改成功!");
+                window.location.href='/teacher/register'
             }
-            content.innerHTML = str;
-        }
-    });
-    $.ajax({
-        type: "get",
-        url: "school/city?province="+$("#pro"),
-        data:'province='+$("#pro"),
-        success: function(data) {
-            alert("success");
-            var content = document.getElementById('city');
-            content.empty();
-            var str = "";
-            for(i=0;i<data.length;i++){
-                str = str +"<option>"+data[i]+"</option>"
+        },
+        statusCode: {
+            409: function () {
+                alert("与其他学校重复");
             }
-            content.innerHTML = str;
-        }
-    });
-}
-
-function city(){
-    $.ajax({
-        type: "get",
-        url: "school/city?province="+$("#pro"),
-        data:'province='+$("#pro"),
-        success: function(data) {
-            alert("success");
-            var content = document.getElementById('city');
-            content.empty();
-            var str = "";
-            for(i=0;i<data.length;i++){
-                str = str +"<option>"+data[i]+"</option>"
-            }
-            content.innerHTML = str;
         }
     });
 }
